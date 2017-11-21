c54feedbb45ba0eb5200306ae6d408d891eb510f
joefritsch <jf@3mo.at>
2017-11-21 03:08:01 +0100

    update Text Domain

--- trm_wp_functions.php
+++ trm_wp_functions.php
@@ -15,7 +15,7 @@
  * Author URI:        http.//treemotion.at
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       trm_functions
+ * Text Domain:       trm_wp_functions
  */
 
 // If this file is called directly, abort.
