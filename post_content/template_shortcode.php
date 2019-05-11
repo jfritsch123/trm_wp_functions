19b246a7c2410079cdb242bad0e69e34a904dda6
joefritsch <jf@3mo.at>
2019-05-11 13:49:52 +0200

    new shortcode: template-content

--- post_content/template_shortcode.php
+++ post_content/template_shortcode.php
@@ -7,11 +7,12 @@
 
 function trm_template_shortcode($atts) {
 	$args = shortcode_atts( array(
-		'template' => 'template.php'
+		'template' => 'template.php',
+		'template-dir' => 'shortcode-templates/'
 	), $atts, 'template-content' );
 
 	ob_start();
-	include get_stylesheet_directory_uri().$atts['template'];
+	include get_stylesheet_directory_uri().'/'.$atts['template-dir'].$atts['template'];
 	$contents = ob_get_contents();
 	ob_end_clean();
 	return $contents;
