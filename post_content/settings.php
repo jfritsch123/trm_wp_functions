f064f662c8deee12e07bbe237c08c39abd7a9132
joefritsch <jf@3mo.at>
2019-05-11 13:44:21 +0200

    new shortcode: template-content

--- post_content/settings.php
+++ post_content/settings.php
@@ -8,6 +8,9 @@
 include_once TRM_POST_CONTENT_PLUGIN_DIR_PATH.'post_content/post_content_shortcode.php';
 add_shortcode('post-content','trm_post_content_shortcode');
 
+include_once TRM_POST_CONTENT_PLUGIN_DIR_PATH.'post_content/template_shortcode.php';
+add_shortcode('template-content','trm_template_shortcode');
+
 //add_action( 'wp_enqueue_scripts', 'trm_wp_functions_enqueue' );
 function trm_functions_enqueue() {
 	// trm_gallery styles
---
+++ post_content/template_shortcode.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * kinderschutz.
+ * joefritsch
+ * 2019-05-11
+ */
+
+function trm_template_shortcode($atts) {
+	$args = shortcode_atts( array(
+		'template' => 'template.php'
+	), $atts, 'template-content' );
+
+	ob_start();
+	include get_stylesheet_directory_uri().$atts['template'];
+	$contents = ob_get_contents();
+	ob_end_clean();
+	return $contents;
+}
